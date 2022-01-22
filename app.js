be79ee6ad55ae207f299bb39efd36e4c5802dafa
Sansar Jaiswal <hjaiswal34@gmail.com>
2022-01-22 16:28:23 +0530

    Added gitignore

--- app.js
+++ app.js
@@ -145,5 +145,5 @@ app.get("/about", function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Server started on port 3000");
+    console.log("Server started");
 });
